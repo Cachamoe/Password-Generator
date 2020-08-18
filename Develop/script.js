abf475d3d014fc6a357bc8044b4cea4e113fa81b
Caleb Chaney <calebc347@gmail.com>
2020-08-18 12:21:44 -0500

    updated html and javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,43 +1,59 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+  // Arrays for randomization 
 
+  var generateBtn = document.querySelector("#generate");
+  generateBtn.addEventListener("click", generatePassword);
+  function generatePassword() {
 
-// Write password to the #password input
-function writePassword() 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // Declare valuables    
 
-  passwordText.value = password;
+  var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+  var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  var numbers = ["0123456789"];
+  var special = ["!@#$%^&*()_+~`|}{[]:;?><,./-="];
 
-  
-// Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword); 
+  // Prompt for length of password
 
-
-// Prompt for length of password
   var passlength = prompt("Password length between 8-128 characters"); {
     if (passlength < 8 || passlength > 128) alert("Error");
     else (console.log(passlength));
-  }   
+  }
+  // Get id's for imputs
 
+  var lowercaseInput = document.getElementById("lowercase");
+  var uppercaseInput = document.getElementById("uppercase");
+  var punctuationInput = document.getElementById("punctuation");
+  var numbersInput = document.getElementById("numbers");
 
-// Prompt for character types
-  var passtype = prompt("Chose lowercase, uppercase, numbers, special, or characters for all"); {
-   if (passtype === ([characters], [lowercase], [uppercase], [numbers], [special])) console.log(passtype);
-    else alert("Error");
+  var userpassword = "";
+  var generatingpassword = "";
+
+  // If statements to get user selection
+
+  if (lowercaseInput.checked) {
+    userpassword += lowercase;
+  }
+  if (uppercaseInput.checked) {
+    userpassword += uppercase;
+  }
+  if (punctuationInput.checked) {
+    userpassword += special;
+  }
+  if (numbersInput.checked) {
+    userpassword += numbers;
   }
 
-// Criteria for generating password
-  if (passlength && passtype === true); {
-    writePassword();
+
+  // For loop that connects to passlength (concotonate with random userpassword)
+
+  for (var i = 0; i < passlength; i++) {
+    generatingpassword += userpassword.charAt(Math.floor(Math.random() * userpassword.length));
+
   }
 
+  // Display value of new password
 
-// Arrays for randomization 
-var characters = [lowercase, uppercase, numbers, special]
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "/", "<", ">", "~", "+", "-", "_", "=", ";", ":"] 
+  document.getElementById("password").value = generatingpassword;
 
 
+  return generatingpassword;
+  }
